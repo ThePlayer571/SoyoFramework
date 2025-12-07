62d25866b567e243ad05c3bad0e6c56de19e1bc0
ThePlayer571 <ThePlayer571@qq.com>
2025-12-07 10:32:18 +0800

    【更新】 LogKit

---
+++ Assets/SoyoFramework/Framework/Runtime/LogKit/UnityLogExtension.cs
@@ -0,0 +1,22 @@
+using Debug = UnityEngine.Debug;
+
+namespace SoyoFramework.Framework.Runtime.LogKit
+{
+    public static class UnityLogExtension
+    {
+        public static void LogInfo(this object self)
+        {
+            Debug.Log(self);
+        }
+
+        public static void LogWarning(this object self)
+        {
+            Debug.LogWarning(self);
+        }
+
+        public static void LogError(this object self)
+        {
+            Debug.LogError(self);
+        }
+    }
+}
