6951698672eb6577eaa4960302f19bb60e840176
ThePlayer571 <ThePlayer571@qq.com>
2025-12-11 11:21:27 +0800

    【更新】 ActionKit: AutoUpdater

--- Assets/SoyoFramework/OptionalKits/ActionKit/Runtime/AutoUpdater.cs
+++ Assets/SoyoFramework/OptionalKits/ActionKit/Runtime/AutoUpdater.cs
@@ -1,7 +1,45 @@
+using System;
+
 namespace SoyoFramework.OptionalKits.ActionKit.Runtime
 {
     public class AutoUpdater
     {
-        
+        private Action _onEnter;
+        private Action _onUpdate;
+        private Action _onExit;
+
+        public void StartUpdate()
+        {
+            _onEnter?.Invoke();
+            ActionKit.RunOnUpdate(_onUpdate);
+        }
+
+        public void StopUpdate()
+        {
+            ActionKit.StopRunOnUpdate(_onUpdate);
+            _onExit?.Invoke();
+        }
+
+        public AutoUpdater()
+        {
+        }
+
+        public AutoUpdater WithOnEnter(Action onEnter)
+        {
+            _onEnter += onEnter;
+            return this;
+        }
+
+        public AutoUpdater WithOnUpdate(Action onUpdate)
+        {
+            _onUpdate += onUpdate;
+            return this;
+        }
+
+        public AutoUpdater WithOnExit(Action onExit)
+        {
+            _onExit += onExit;
+            return this;
+        }
     }
 }
