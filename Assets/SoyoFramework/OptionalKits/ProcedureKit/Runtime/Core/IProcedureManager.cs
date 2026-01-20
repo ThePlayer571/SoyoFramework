7d2683282e08e90e7c5638b5519049e987686027
ThePlayer571 <ThePlayer571@qq.com>
2026-01-20 13:09:59 +0800

    【更新】 ProcedureKit结构优化

--- Assets/SoyoFramework/OptionalKits/ProcedureKit/Runtime/Core/IProcedureManager.cs
+++ Assets/SoyoFramework/OptionalKits/ProcedureKit/Runtime/Core/IProcedureManager.cs
@@ -7,13 +7,12 @@ using SoyoFramework.OptionalKits.ProcedureKit.Runtime.DataClasses;
 
 namespace SoyoFramework.OptionalKits.ProcedureKit.Runtime.Core
 {
-    [BelongsToLayer("System + Model + Service")]
-    public interface IProcedureManager<TProcedureId, TTagId>
+    public interface IProcedureModel<TProcedureId, TTagId>
     {
-        // [Model]
         // 数据
         TProcedureId CurrentProcedure { get; }
         ProcedureCheckMode CheckMode { get; set; }
+
         bool IsChangingProcedure { get; }
 
         // 标签
@@ -22,20 +21,26 @@ namespace SoyoFramework.OptionalKits.ProcedureKit.Runtime.Core
         bool CurrentHasTag(TTagId tag);
         IReadOnlyCollection<TTagId> GetCurrentTags();
 
-        // [SendEvent]
+
+        // 事件
+        EasyEvent<TProcedureId, ProcedureChangeStage> OnProcedureChange { get; }
+
+        IUnRegister Register(TProcedureId procedureId, ProcedureChangeStage stage,
+            Action<ProcedureChangeInfo> callback);
+    }
+
+    public interface IProcedureService<TProcedureId>
+    {
         // 流程切换
         UniTask ChangeProcedure(TProcedureId procedureId, ProcedureChangeInfo.ProcedureChangeParas paras);
         UniTask ChangeProcedure(TProcedureId procedureId, params (string, object)[] paras);
 
-        // [RegisterEvent]
-        // 流程订阅
-        IUnRegister Register(TProcedureId procedureId, ProcedureChangeStage stage,
-            Action<ProcedureChangeInfo> callback);
-
-        EasyEvent<TProcedureId, ProcedureChangeStage> OnProcedureChange { get; }
-
-        // [Service]
-        // 延迟切换
         void AddAwait(UniTask task);
+    }
+
+    public interface IProcedureManager<TProcedureId, TTagId> :
+        IProcedureModel<TProcedureId, TTagId>,
+        IProcedureService<TProcedureId>
+    {
     }
 }
