f8e39f7a6e52e334f741a42980d4c3194c00d3a3
ThePlayer571 <ThePlayer571@qq.com>
2026-03-02 14:20:11 +0800

    【更新】 获取到null的层级会报错 移除废物方法RegisterModuleRef

--- Assets/SoyoFramework/Framework/Runtime/Core/Architecture.cs
+++ Assets/SoyoFramework/Framework/Runtime/Core/Architecture.cs
@@ -117,6 +117,12 @@ namespace SoyoFramework.Framework.Runtime.Core
 
         public void RegisterModule<T>(T module) where T : class, IModule
         {
+            if (module == null)
+            {
+                $"注册失败，注册的模块不能为null: {typeof(T).Name}".LogError();
+                return;
+            }
+
             module.AttachedArchitecture = this;
             _container.Register<T>(module);
 
@@ -128,65 +134,36 @@ namespace SoyoFramework.Framework.Runtime.Core
             }
         }
 
-        /// <summary>
-        /// 使用IOC中已注册的类型作为新类型进行注册（SuperLayer语法糖）
-        /// </summary>
-        /// <typeparam name="T">新注册的类型</typeparam>
-        /// <typeparam name="TRef">IOC中已存在的类型</typeparam>
-        protected void RegisterModuleRef<T, TRef>()
-            where T : class, IModule
-            where TRef : class, IModule
-        {
-            if (_container.Get<TRef>() is T module)
-            {
-                RegisterModule(module);
-            }
-            else
-            {
-                $"尝试将已注册的类型 {typeof(TRef)} 作为新类型 {typeof(T)} 注册时失败，原因是类型不匹配".LogError();
-            }
-        }
-
         public T GetModule<T>() where T : class, IModule
         {
-            return _container.Get<T>();
+            var module = _container.Get<T>();
+            if (module == null)
+            {
+                $"尝试获取未注册的模块: {typeof(T).Name}".LogError();
+                return null;
+            }
+
+            return module;
         }
 
         public void RegisterModel<T>(T model) where T : class, IModel
         {
-            model.AttachedArchitecture = this;
-            _container.Register<T>(model);
-
-            // 如果是在Architecture初始化后注册的Module，直接初始化
-            if (Inited)
-            {
-                model.PreInit();
-                model.Init();
-            }
+            RegisterModule(model);
         }
 
         public void RegisterSystem<T>(T system) where T : class, ISystem
         {
-            system.AttachedArchitecture = this;
-            _container.Register<T>(system);
-
-            // 如果是在Architecture初始化后注册的Module，直接初始化
-            if (Inited)
-            {
-                system.PreInit();
-                system.Init();
-            }
-        }
-
-
-        public T GetSystem<T>() where T : class, ISystem
-        {
-            return _container.Get<T>();
+            RegisterModule(system);
         }
 
         public T GetModel<T>() where T : class, IModel
         {
-            return _container.Get<T>();
+            return GetModule<T>();
+        }
+
+        public T GetSystem<T>() where T : class, ISystem
+        {
+            return GetModule<T>();
         }
 
         public IUnRegister RegisterEvent<T>(Action<T> onEvent) where T : IEvent
@@ -207,38 +184,22 @@ namespace SoyoFramework.Framework.Runtime.Core
 
         public void RegisterTool<T>(T tool) where T : class, ITool
         {
-            tool.AttachedArchitecture = this;
-            _container.Register<T>(tool);
-
-            // 如果是在Architecture初始化后注册的Module，直接初始化
-            if (Inited)
-            {
-                tool.PreInit();
-                tool.Init();
-            }
+            RegisterModule(tool);
         }
 
         public T GetTool<T>() where T : class, ITool
         {
-            return _container.Get<T>();
+            return GetModule<T>();
         }
 
         public void RegisterVController<T>(T viewController) where T : class, IVController
         {
-            viewController.AttachedArchitecture = this;
-            _container.Register<T>(viewController);
-
-            // 如果是在Architecture初始化后注册的Module，直接初始化
-            if (Inited)
-            {
-                viewController.PreInit();
-                viewController.Init();
-            }
+            RegisterModule(viewController);
         }
 
         public T GetVController<T>() where T : class, IVController
         {
-            return _container.Get<T>();
+            return GetModule<T>();
         }
 
         public void SendCommand(ICommand command)
